fa472474ddb7008cc70a188915cef1f9eaa473d6
Alexon Rodrigo <Alexon Rodrigo@Not_Alexon>
2017-03-12 20:52:28 -0300

    Alteração conexão git.

--- ProjetoVersionado/src/br/univel/Principal.java
+++ ProjetoVersionado/src/br/univel/Principal.java
@@ -3,6 +3,7 @@ package br.univel;
 public class Principal {
 
 	public static void main(String[] args) {
+		System.out.println("------GitHub-------");
 		String msg = "Hello World!";
 		System.out.println("-------------------");
 		System.out.println(msg);
